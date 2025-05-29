58f2ecd5c071ff14d711aa6c177a804c11ef95e1
David <david@gmail.com>
2025-05-29 11:36:07 -0500

    actualizar

---
+++ config/database.php
@@ -0,0 +1,35 @@
+<?php
+    
+    //Config connection
+    $host       = "aws-0-us-east-1.pooler.supabase.com";
+    $port       = "6543";
+    $dbname     = "postgres";
+    $user       = "postgres.dkrdwirmatkvepyjezsb";
+    $password   = "unicesmag@@";
+
+    /* 
+    $host       = "localhost";
+    $port       = "5432";
+    $dbname     = "schoolar";
+    $user       = "postgres";
+    $password   = "unicesmag";
+    */
+
+    //create connection 
+    $conn = pg_connect("
+        host=$host
+        port=$port
+        dbname=$dbname
+        user=$user
+        password=$password
+    
+    ");
+
+    if (!$conn){
+        //die("connection error: " . pg_last_error());
+    }else{
+       // echo "Succcess connection";
+    }
+
+   // pg_close();
+?>
